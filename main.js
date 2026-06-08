62ee62c7ff664f03dd0dab1b946e2fe7169334b3
Erick <correiaerick1234@gmail.com>
2026-06-08 12:08:44 -0300

    tentativas

---
+++ main.js
@@ -0,0 +1,20 @@
+/*
+let numero = Number(prompt("Digite um numero para fazer a tabuada do 1 até 10:"))
+let tabuada = 1
+
+while (tabuada < 11) {
+    let resultador = numero * tabuada;
+    console.log(resultador)
+    tabuada = tabuada + 1;
+}
+*/
+let num = 1
+for(let tentativas = 3 ; tentativas >= 1; tentativas = tentativas - 1){
+    ten = Number(prompt("digite um numero. obs vc tem 3 tentativas"))
+    if (ten == num) {
+        alert("ganhou")
+        tentativas = 0
+    }else{
+        alert("perdeu")
+    }
+}
